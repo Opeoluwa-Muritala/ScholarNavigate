cc44edcce9487481797c47e90a454b3705c4d667
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2024-05-23 12:49:05 +0100

    Project Setup

---
+++ app/src/main/java/com/example/scholarnavigate/ui/screens/signIn/SignInScreen.kt
@@ -0,0 +1,20 @@
+package com.example.scholarnavigate.ui.screens.signIn
+
+import androidx.compose.ui.unit.dp
+
+//@Composable
+//fun SignInScreen(){
+//    Column(){
+//        Button(
+//            onClick = {}
+//        ){
+//            Image(
+//                painterResource(R.drawable.google_icon),
+//                "Sign in",
+//                modifier = Modifier.size(30.dp)
+//            )
+//            Text("Sign In")
+//        }
+//
+//    }
+//}
